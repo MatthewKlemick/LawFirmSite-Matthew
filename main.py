423a796aacef98e735b808702c1882934a398e09
MatthewK <matt.klemick@xtra.co.nz>
2021-03-11 12:00:22 +1300

    About Section Completed

--- main.py
+++ main.py
@@ -10,4 +10,6 @@ main.config['Images'] = images
 @main.route('/')
 def Main():
     Image_name1 = os.path.join(main.config['Images'], 'logo.jpg')
-    return render_template('mainpage.html', I1 = Image_name1)
+    Image_name2 = os.path.join(main.config['Images'], 'business-woman.jpg')
+    Image_name3 = os.path.join(main.config['Images'], 'business-man.jpg')
+    return render_template('mainpage.html', I1 = Image_name1, I2 = Image_name2, I3 = Image_name3)
